3657fdd08e934a00137c3336ebae2074998fcca9
Miguel Yáñez <60891165+Yanezzz12@users.noreply.github.com>
2024-11-02 22:18:28 -0600

    Dynamixel

---
+++ DiseñoPCB/ControlDynamixelESP32/ProgramaControl.ino/ProgramaControl.ino.ino
@@ -0,0 +1,22 @@
+//https://www.youtube.com/watch?v=aYE_O_ZF2ac
+#include <AX12A.h>
+
+#define DirectionPin (35u)
+#define BaudRate (1000000ul)
+#define ID (1u)
+
+void setup() {
+  delay(1000);  // Give time for Dynamixel to start on power-up
+  ax12a.begin(BaudRate, DirectionPin, &Serial);
+  ax12a.setEndless(ID, ON);
+}
+
+void loop() {
+  ax12a.ledStatus(ID, ON);
+  ax12a.turn(ID, LEFT, 300);
+  delay(5000);
+
+  ax12a.ledStatus(ID, OFF);
+  ax12a.turn(ID, RIGHT, 500);
+  delay(5000);
+}
